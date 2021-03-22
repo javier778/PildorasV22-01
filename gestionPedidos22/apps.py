c3b38a3955a5c8243dde0c347c03f5d78573e8a2
javier778 <javier.sols@web.de>
2021-03-22 17:15:32 +0100

    Primera version de la aplicacion movida a github

---
+++ gestionPedidos22/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Gestionpedidos22Config(AppConfig):
+    name = 'gestionPedidos22'
---
+++ gestionPedidos22/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from gestionPedidos22.models import Articulos
+
+# Create your views here.
+
+def busqueda_productos(request):
+
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+
+    if request.GET["prd"]:
+
+        #mensaje="Artículo buscado: %r" %request.GET["prd"]
+        producto=request.GET["prd"]
+
+        articulos=Articulos.objects.filter(nombre__icontains=producto)
+
+        return render(request, "resultados_busqueda.html", {"articulos":articulos, "query":producto})
+    else:
+        mensaje="No has introducido nada"
+
+    return HttpResponse(mensaje)
